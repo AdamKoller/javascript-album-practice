8ed488f80b25e7e760f925913f720b279e681990
Adam Koller <41529662+AdamKoller@users.noreply.github.com>
2019-06-29 21:41:46 -0500

    started project

---
+++ index.js
@@ -0,0 +1,16 @@
+// click album
+
+
+// show user the selected album(make a class selected to the album div)
+
+
+
+// create aray then add the selected album to that array
+
+
+
+//  find the selected albums total
+
+
+
+// save button click notification
