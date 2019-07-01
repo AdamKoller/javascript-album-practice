d9da5c8b077ebb4c84b423d901669fb0a0c2d151
Adam Koller <41529662+AdamKoller@users.noreply.github.com>
2019-06-30 21:47:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 // click album
-
-
 // show user the selected album(make a class selected to the album div)
-
-
-
 // create aray then add the selected album to that array
-
-
-
 //  find the selected albums total
-
-
-
 // save button click notification
+
+
+
 var notify = document.querySelector('.notification')
 var albums = document.querySelectorAll('.album')
 var itemsTotal = document.querySelector('.items-total')
@@ -25,8 +17,26 @@ var i = 0
 while (i < albums.length) {
   albums[i].onclick = function(e) {
     var albumTitle = this.querySelector('.title').textContent
-    console.log(albumTitle);
+    if (this.classList.contains('selected') !== true) {
+      this.classList.add('selected');
+      albumsSelected.push(albumTitle);
+    } else {
+      this.classList.remove('selected');
+      albumsSelected = albumsSelected.filter(function(item) {
+        return item !== albumTitle
+      })
+    }
+    console.log(i);
   }
-  console.log(i);
   i++;
+}
+
+saveButton.onclick = function() {
+  itemsTotal.textContent = albumsSelected.length + ' Items Saved';
+  notify.classList.add('active');
+
+  setTimeout(function() {
+    notify.classList.remove('active')
+  }, 2000)
+  console.log('Saved');
 }
